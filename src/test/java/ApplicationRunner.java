5e4a1d170ff7c9828e0880683ce8409a97f05284
Nico769 <ncl.lnd769@gmail.com>
2022-01-16 16:31:41 +0100

    First end-to-end test
    Implement just enough logic in `ApplicationRunner` and `FakeAuctionServer` to see the test fail (due to the missing top-level frame)

--- src/test/java/ApplicationRunner.java
+++ src/test/java/ApplicationRunner.java
@@ -1,10 +1,38 @@
 public class ApplicationRunner {
-    public void startBiddingIn(FakeAuctionServer auction) {
+    public static final String XMPP_HOSTNAME = "localhost";
+    public static final String SNIPER_ID = "sniper";
+    public static final String SNIPER_PASSWORD = "sniper";
+    private AuctionSniperDriver driver;
+
+    public void startBiddingIn(final FakeAuctionServer auction) {
+        Thread thread = new Thread("Test application") {
+            @Override
+            public void run() {
+                try {
+                    Main.main(XMPP_HOSTNAME, SNIPER_ID, SNIPER_PASSWORD, auction.getItemId());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+
+        };
+        thread.setDaemon(true);
+        thread.start();
+        // 1 second timeout for finding frames and components
+        driver = new AuctionSniperDriver(1000);
+        driver.showsSniperStatus(Main.STATUS_JOINING);
+
     }
 
+
     public void showsSniperHasLostAuction() {
+        driver.showsSniperStatus(Main.STATUS_LOST);
     }
 
     public void stop() {
+        if (driver != null) {
+            driver.dispose();
+        }
     }
 }

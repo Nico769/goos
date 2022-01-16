e9cbcd0dd7ee3d3bc5cc193b20819337a9445789
Nico769 <ncl.lnd769@gmail.com>
2022-01-16 17:59:55 +0100

    First end-to-end test - PASS
    Implement logic enabling the Sniper to receive a response from the auction and display the outcome on the UI.

--- src/main/java/auctionsniper/Main.java
+++ src/main/java/auctionsniper/Main.java
@@ -2,7 +2,6 @@ package auctionsniper;
 
 import auctionsniper.ui.MainWindow;
 import org.jivesoftware.smack.Chat;
-import org.jivesoftware.smack.MessageListener;
 import org.jivesoftware.smack.XMPPConnection;
 import org.jivesoftware.smack.XMPPException;
 import org.jivesoftware.smack.packet.Message;
@@ -23,6 +22,8 @@ public class Main {
     private static final int ARG_PASSWORD = 2;
     private static final int ARG_ITEM_ID = 3;
     private MainWindow ui;
+    @SuppressWarnings("unused")
+    private Chat notToBeGCd;
 
     public Main() throws Exception {
         startUserInterface();
@@ -30,23 +31,25 @@ public class Main {
 
     public static void main(String... args) throws Exception {
         Main main = new Main();
-        XMPPConnection connection = connectTo(args[ARG_HOSTNAME], args[ARG_USERNAME], args[ARG_PASSWORD]);
-        Chat chat = connection.getChatManager().createChat(auctionId(args[ARG_ITEM_ID], connection),
-                (MessageListener) (aChat, message) -> {
-                    // nothing yet
-                });
-        chat.sendMessage(new Message());
+        main.joinAuction(connection(args[ARG_HOSTNAME], args[ARG_USERNAME], args[ARG_PASSWORD]), args[ARG_ITEM_ID]);
     }
 
     private static String auctionId(String itemId, XMPPConnection connection) {
         return String.format(AUCTION_ID_FORMAT, itemId, connection.getServiceName());
     }
 
-    private static XMPPConnection connectTo(String hostname, String username, String password) throws XMPPException {
+    private static XMPPConnection connection(String hostname, String username, String password) throws XMPPException {
         XMPPConnection connection = new XMPPConnection(hostname);
         connection.connect();
         connection.login(username, password, AUCTION_RESOURCE);
         return connection;
+    }
+
+    private void joinAuction(XMPPConnection connection, String itemId) throws XMPPException {
+        final Chat chat = connection.getChatManager().createChat(auctionId(itemId, connection),
+                (aChat, message) -> SwingUtilities.invokeLater(() -> ui.showStatus(STATUS_LOST)));
+        this.notToBeGCd = chat;
+        chat.sendMessage(new Message());
     }
 
     private void startUserInterface() throws Exception {

407063981b50812e43a8eff10ad197661bbe11a2
Nico769 <ncl.lnd769@gmail.com>
2022-01-16 16:56:02 +0100

    First end-to-end test
    Implement logic to show the status JLabel. The test still fails, because the auction does not receive a Join request from the Sniper.

--- src/main/java/auctionsniper/ui/MainWindow.java
+++ src/main/java/auctionsniper/ui/MainWindow.java
@@ -3,12 +3,25 @@ package auctionsniper.ui;
 import auctionsniper.Main;
 
 import javax.swing.*;
+import javax.swing.border.LineBorder;
+import java.awt.*;
 
 public class MainWindow extends JFrame {
+    private final JLabel sniperStatus = createLabel(Main.STATUS_JOINING);
+
     public MainWindow() {
         super("Auction sniper");
         setName(Main.MAIN_WINDOW_NAME);
+        add(sniperStatus);
+        pack();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
+    }
+
+    private static JLabel createLabel(String initialText) {
+        JLabel result = new JLabel(initialText);
+        result.setName(Main.SNIPER_STATUS_NAME);
+        result.setBorder(new LineBorder(Color.BLACK));
+        return result;
     }
 }

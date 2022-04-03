ebeef0e4fde08670e60c44e6ec72c551c731de90
Nico769 <ncl.lnd769@gmail.com>
2022-04-03 11:25:02 +0200

    Fourth UT for AuctionSniperTest - PASS
    - Implemented the fourth unit test for the AuctionSniperTest. The test checks whether the sniper announces it has lost when the auction closes while it is bidding;
    - Renamed AuctionSniperTest#reportsLostWhenAuctionCloses to AuctionSniperTest#reportsLostIfAuctionClosesImmediately

--- src/test/java/auctionsniper/AuctionSniperTest.java
+++ src/test/java/auctionsniper/AuctionSniperTest.java
@@ -2,6 +2,7 @@ package auctionsniper;
 
 import org.jmock.Expectations;
 import org.jmock.Mockery;
+import org.jmock.States;
 import org.jmock.junit5.JUnit5Mockery;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
@@ -15,12 +16,26 @@ class AuctionSniperTest {
     private final SniperListener sniperListener = context.mock(SniperListener.class);
     private final Auction auction = context.mock(Auction.class);
     private final AuctionSniper sniper = new AuctionSniper(auction, sniperListener);
+    private final States sniperState = context.states("sniper");
 
     @Test
-    void reportsLostWhenAuctionCloses() {
+    void reportsLostIfAuctionClosesImmediately() {
         context.checking(new Expectations() {{
             atLeast(1).of(sniperListener).sniperLost();
         }});
+        sniper.auctionClosed();
+    }
+
+    @Test
+    void reportsLostIfAuctionClosesWhenBidding() {
+        context.checking(new Expectations() {{
+            ignoring(auction);
+            allowing(sniperListener).sniperBidding();
+            then(sniperState.is("bidding"));
+            atLeast(1).of(sniperListener).sniperLost();
+            when(sniperState.is("bidding"));
+        }});
+        sniper.currentPrice(123, 45, PriceSource.FROM_OTHER_BIDDER);
         sniper.auctionClosed();
     }
 
